22a2e6f5dd15ba80167898add196e44c71f28932
imtiyaz hirani <imtiyazhirani@imtiyazs-MacBook-Air.local>
2025-02-05 11:30:13 +0530

    05-02-25

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 //const API_BASE_URL = "http://localhost:5005";  
-const API_BASE_URL = "http://52.66.237.57:5005";  
+const API_BASE_URL = "https://52.66.237.57:5005";  
 
 export default API_BASE_URL;
