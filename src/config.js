db1dfd2b913b6f785c892e5b264a4796c8fbc35e
imtiyaz hirani <imtiyazhirani@imtiyazs-MacBook-Air.local>
2025-02-05 11:37:42 +0530

    05-02-25

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 //const API_BASE_URL = "http://localhost:5005";  
-const API_BASE_URL = "https://52.66.237.57:5005";  
+const API_BASE_URL = "https://api.techskillsit.com";  
 
 export default API_BASE_URL;
